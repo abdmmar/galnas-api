042729d0b88735e81b7c3833898f7e7b63abdc7a
abdmmar <opowae909@gmail.com>
2023-12-01 19:31:23 +0700

    refactor: move configs inside src

--- src/configs/postgresql.ts
+++ src/configs/postgresql.ts
@@ -1,6 +1,6 @@
 import client from 'pg';
 
-const pool = new client.Pool({
+const db = new client.Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
@@ -8,4 +8,4 @@ const pool = new client.Pool({
   port: parseInt(process.env.POSTGRES_PORT as string),
 });
 
-export { pool };
+export { db };
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import { koaBody } from 'koa-body'
 
 import * as CollectionHandler from '@/handlers/collection.js'
 
+const PORT = parseInt(process.env.PORT as string)
+
 const router = new Router()
 const app = new Koa()
 
 app.use(koaBody())
 
-router.get('/', CollectionHandler.getCollections)
+router.get('/api/collection', CollectionHandler.getCollections)
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000")
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
 })
